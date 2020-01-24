8179711976865456814d37917cb09954a912b3dd
Christian Lee <christian.r.lee@gmail.com>
2020-01-24 16:54:05 -0500

    Update display_script.m

--- display_script.m
+++ display_script.m
@@ -10,3 +10,11 @@ load(file)
 vars = whos;
 
 types = {vars.class};
+
+for step = 1:size(types, 2);
+    if strcmp(types(1, step) , 'uint16') == 1
+        selection = step;
+    else
+    end
+end
+
